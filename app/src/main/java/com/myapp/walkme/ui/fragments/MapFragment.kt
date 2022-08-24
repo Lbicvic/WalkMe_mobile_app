7836e820a39593dc7d90912d1a118b6feaa4d55d
Lbicvic <leopold.bicvic@gmail.com>
2022-08-24 15:32:13 +0200

    add map

--- app/src/main/java/com/myapp/walkme/ui/fragments/MapFragment.kt
+++ app/src/main/java/com/myapp/walkme/ui/fragments/MapFragment.kt
@@ -5,9 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.myapp.walkme.R
 import com.myapp.walkme.databinding.FragmentMapBinding
 
-class MapFragment: Fragment() {
+class MapFragment: Fragment(), OnMapReadyCallback {
     lateinit var binding: FragmentMapBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +22,19 @@ class MapFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMapBinding.inflate(layoutInflater)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         return binding.root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val currentPosition = LatLng(45.55111, 18.69389)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(currentPosition)
+                .title("Dog Location")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition,12f))
     }
 }
